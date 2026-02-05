aa67ceac0d19abe306a105c78b3a735a517b09d2
teodoraspasojevic <spasojevic.teodora@gmail.com>
2026-02-05 17:38:08 +0000

    Deploying to gh-pages from @ teodoraspasojevic/academic-webpage@dbd4ccf33f81cbf7284095e50cca3f01059189cc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/academic-webpage/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/academic-webpage/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
